283e26aea067efb3d5ac1a6d6bb918c969dd1d5a
dganai <dinaganai@gmail.com>
2022-02-10 18:05:10 +0000

    Error handling for process.argv

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -8,14 +8,14 @@ const pool = new Pool({
 });
 
 pool.query(`
-SELECT id, name, cohort_id
+SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students
 JOIN cohorts ON cohorts.id = cohort_id
 WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT 5;
+LIMIT ${process.argv[3] || 5};
 `)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
   })
-});
+}).catch(err => console.error('query error', err.stack));
