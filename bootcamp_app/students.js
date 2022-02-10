88f431ad250a9af82645aa8dd4a203401e30d4bf
dganai <dinaganai@gmail.com>
2022-02-10 17:55:55 +0000

    Querying database using pool.query function

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -6,3 +6,13 @@ const pool = new Pool({
   host: 'localhost',
   database: 'bootcampx'
 });
+
+pool.query(`
+SELECT id, name, cohort_id
+FROM students
+LIMIT 5;
+`)
+.then(res => {
+  console.log(res);
+})
+.catch(err => console.error('query error', err.stack));
