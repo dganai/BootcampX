88d7ea6b73ee62189eff4e5763a37437805ceb78
dganai <dinaganai@gmail.com>
2022-02-11 14:09:08 +0000

    eslint fixes

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -8,19 +8,19 @@ const pool = new Pool({
 });
 
 const queryString = `
-SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-FROM students 
-JOIN cohorts ON students.cohort_id = cohort.id
-WHERE cohorts.name LIKE $1
-LIMIT $2
+  SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+  FROM students 
+  JOIN cohorts ON students.cohort_id = cohort.id
+  WHERE cohorts.name LIKE $1
+  LIMIT $2
 `;
 const cohortName = process.argv[2];
 const limit =  process.argv[3] || 5;
-const values = [`%${cohortName}%`, limit]
+const values = [`%${cohortName}%`, limit];
 
 pool.query(queryString, values)
-.then(res => {
-  res.rows.forEach(user => {
-    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
-  })
-}).catch(err => console.error('query error', err.stack));
+  .then(res => {
+    res.rows.forEach(user => {
+      console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+    });
+  }).catch(err => console.error('query error', err.stack));
